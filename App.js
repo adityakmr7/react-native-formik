4494b985e937879b536b52362461585cba5b6d29
adityakmr7 <adityakmr9672@gmail.com>
2021-03-06 23:04:27 +0530

    initial setup

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Dimensions, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
+const { width:wWidth,height:wHeight} = Dimensions.get('window');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="User Name"
+        />
+      </View>
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Password"
+        />
+      </View>
+      <View style={styles.signUpButtonBox}>
+        <TouchableWithoutFeedback>
+          <View style={styles.signUpButton}>
+            <Text>SignUp</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 }
@@ -18,4 +33,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputBox: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#000000',
+    width: wWidth - 40,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10
+    
+  },
+  signUpButtonBox: {
+    marginVertical: 10
+  },
+  signUpButton: {
+    backgroundColor: 'cyan',
+    width: wWidth - 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems:'center'
+  }
 });
